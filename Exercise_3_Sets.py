ee363e24965e8954da32fd9e055385270fa65b74
David Wu <david204code@gmail.com>
2022-02-04 12:31:04 +0000

    Correction it is exercise 3 not 4, in the middle

---
+++ Exercise_3_Sets.py
@@ -0,0 +1,31 @@
+#CODE1: Create an empty SET
+set1 = set()
+print("CODE1:")
+print(f"set1 = {set1}")
+print(f"data type = {type(set1)}")
+print(f"length = {len(set1)}")
+print()
+
+#CODE2: Create SET with 1 item
+set2 = {"cloudacademy"}
+print("CODE2:")
+print(f"set2 = {set2}")
+print(f"data type = {type(set2)}")
+print(f"length = {len(set2)}")
+print()
+
+#CODE3 Create SET with multiple items of the same type
+set3 = {1, 2, 3, 4, 5}
+print("CODE3:")
+print(f"set3 = {set3}")
+print(f"data type = {type(set3)}")
+print(f"length = {len(set3)}")
+print() 
+
+#CODE4: Create SET with multiple items of different types
+set4 = {"cloud", "academy", 1, True, False}
+print("CODE4:")
+print(f"set4 = {set4}")
+print(f"data type = {type(set4)}")
+print(f"length = {len(set4)}")
+print()
