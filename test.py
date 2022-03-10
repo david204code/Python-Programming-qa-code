9d367fcdc1e3c5d03d3d125f1e4e7b58ce8dc719
David Wu <david204code@gmail.com>
2022-03-10 21:16:55 +0000

    test2 commit

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-print ("This is file 1")
-print (50)
+    # print ("This is file 1")
+    # print (50)
 
-def hello_world(s):
-    return print(f"Hello " + s)
-    
-hello_world("David")
+    # def hello_world(s):
+    #     return print(f"Hello " + s)
+        
+    # hello_world("David")
+
+#def greeting():
+#    return 'Peace to you!'
+
+#print(greeting())
 
